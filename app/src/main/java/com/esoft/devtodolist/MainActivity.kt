51417340d4bc5669bc6116021ea0d60a634fcb84
Shutikov Evgeni <shutikov.e@yandex.ru>
2021-08-03 14:49:55 +0700

    Верстка ListActivity

--- app/src/main/java/com/esoft/devtodolist/MainActivity.kt
+++ app/src/main/java/com/esoft/devtodolist/MainActivity.kt
@@ -2,10 +2,23 @@ package com.esoft.devtodolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.esoft.devtodolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.searchBar)
+        supportActionBar!!.title = ""
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
